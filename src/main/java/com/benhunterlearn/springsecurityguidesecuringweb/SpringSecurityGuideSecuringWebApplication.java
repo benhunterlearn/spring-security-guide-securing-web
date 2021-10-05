299c203b2f4ae0ca73503a531fd5d6950e4f120a
Ben Hunter <learntocode@benhunter.me>
2021-10-05 15:20:04 -0500

    Project Generated from Spring Initializr

---
+++ src/main/java/com/benhunterlearn/springsecurityguidesecuringweb/SpringSecurityGuideSecuringWebApplication.java
@@ -0,0 +1,13 @@
+package com.benhunterlearn.springsecurityguidesecuringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityGuideSecuringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityGuideSecuringWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benhunterlearn/springsecurityguidesecuringweb/SpringSecurityGuideSecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benhunterlearn.springsecurityguidesecuringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityGuideSecuringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
